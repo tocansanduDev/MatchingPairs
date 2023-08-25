9de154a94f7d3227145cedc5ba1351b5704ca904
Sandu Tocan <sandu.tocan@tapptitude.com>
2023-08-25 18:55:46 +0300

    Add: project

---
+++ MatchingPairs/MatchingPairs/MatchingPairsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingPairsApp.swift
+//  MatchingPairs
+//
+//  Created by Sandu Tocan on 25.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MatchingPairsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
